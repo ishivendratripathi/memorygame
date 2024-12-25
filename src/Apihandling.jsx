94834239ef916f8973b2808238404ac50e3a6068
Shivendra <shivendra.tripathi@mca.christuniversity.in>
2024-12-25 13:07:23 +0530

    new updates

--- src/Apihandling.jsx
+++ src/Apihandling.jsx
@@ -5,10 +5,23 @@ const Apihandling = () => {
 
   const [data, setData] = useState([]);
 
+
+  const sufflearray = (Array) => {
+    const copyshuffle=[...Array]
+    for(let i=copyshuffle.length-1;i>0;i--){
+      const j = Math.floor(Math.random() * (i+1));
+      [copyshuffle[i],copyshuffle[j]]=[copyshuffle[j],copyshuffle[i]] 
+    }
+
+    return copyshuffle;
+    
+  }
+
   useEffect(() => {
     axios.get("https://digimon-api.vercel.app/api/digimon")
     .then(function (response) {
-      setData(response.data)
+      const newshuffled=sufflearray(response.data)
+      setData(newshuffled)
     })
   },[])
 
@@ -16,12 +29,21 @@ const Apihandling = () => {
     <>
       <div className='text-center font-extrabold text-3xl text-rose-600'>IS YOUR MEMORY GOOD?</div>
       <div className='text-xl text-blue-600'>DON'T TAP THE SAME CARD ELSE YOU WILL LOSE</div>    
-  <div className='flex flex-wrap m-6 gap-6'>{data.map((a)=>
+  <div className='flex flex-wrap m-6 gap-6'>{data.map((a , index)=>
   <>
-  <div className='h-48 w-48 border-2 border-black flex flex-col items-center justify-center p-2 rounded'>
-<img src={a.img} alt="" className='h-24 w-24 object-contain mb-2'/>
-  <div className='text-lg font-medium'>{a.name}</div>
-  </div>
+  <button
+  key={index}
+  className="relative h-48 w-48 border-2 border-black flex flex-col items-center justify-center p-2 rounded overflow-hidden shadow-2xl group hover:text-white hover:shadow-black"
+>
+  <span className="absolute left-0 -ml-2 h-48 w-48 origin-top-right -translate-x-full translate-y-12 -rotate-90 bg-gray-900 transition-all duration-300 group-hover:-rotate-180"></span>
+  <img
+    src={a.img}
+    alt=""
+    className="h-24 w-24 object-contain mb-2 z-10"
+  />
+  <div className="text-lg font-medium z-10">{a.name}</div>
+</button>
+
 
   </>)}</div>
       </>
